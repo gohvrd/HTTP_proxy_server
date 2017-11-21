7f587d2b9bb983f6f07d39f07bb7a6d1bb44158a
LakrimozaSL <Reqiman@mail.ru>
2017-11-22 00:44:00 +0300

    Add files via upload

--- database/Database.cpp
+++ database/Database.cpp
@@ -7,7 +7,72 @@
 
 #define FILE_NAME "Statistic.json"
 
-bool PutInDatabase(Statistic *statistic, unsigned int count)
+void WriteStructInFstream(FILE* file, Statistic oneStruct)
+{
+	fprintf(file, "%s", "{\n");
+	fprintf(file, "%s", oneStruct.InternetProtocol);
+	fprintf(file, "%s", "\n");
+	fprintf(file, "%s", oneStruct.ConnectTime);
+	fprintf(file, "%s", "\n");
+	fprintf(file, "%d", oneStruct.CountSendDate);
+	fprintf(file, "%s", "\n");
+	fprintf(file, "%d", oneStruct.CountReceiveDate);
+	fprintf(file, "%s", "\n}\n");
+
+	return;
+}
+
+unsigned int StructCount()
+{
+	FILE *file;
+
+	if ((file = fopen(FILE_NAME, "rt")) == NULL)
+		return NULL;
+
+	unsigned int structCount(0);
+	char c;
+
+	while (!feof(file))
+		if ((c = fgetc(file)) == '{')
+			structCount++;
+
+	if (fclose(file) == EOF)
+		return false;
+
+	return structCount;
+}
+
+Statistic *ReadStructFromDatabase()
+{
+	unsigned int structCount = StructCount();
+
+	if (structCount == 0)
+		return NULL;
+
+	Statistic *statistic = new Statistic[structCount];
+
+	std::ifstream file(FILE_NAME);
+
+	char buff[20];
+
+	for (int i(0); i < structCount; ++i)
+	{
+		file >> buff >> statistic[i].InternetProtocol >> statistic[i].ConnectTime >> buff;
+
+		statistic[i].ConnectTime[8] = ' ';
+
+		for (int j(0); j < 8; ++j)
+			statistic[i].ConnectTime[j + 9] = buff[j];
+
+		statistic[i].ConnectTime[17] = '\0';
+
+		file >> statistic[i].CountReceiveDate >> statistic[i].CountSendDate >> buff;
+	}
+
+	return statistic;
+}
+
+bool PutAllInDatabase(Statistic *statistic, unsigned int count)
 {
 	FILE *file;
 
@@ -35,48 +100,46 @@ bool PutInDatabase(Statistic *statistic, unsigned int count)
 
 }
 
-
-unsigned int StructCount()
+bool PutInDatabase(Statistic oneStruct)
 {
 	FILE *file;
 
-	if ((file = fopen(FILE_NAME, "rt")) == NULL)
-		return NULL;
-
-	unsigned int structCount(0);
-	char c;
-
-	while (!feof(file))
-		if ((c = fgetc(file)) == '{')
-			structCount++;
-
-	return structCount;
-}
-
-Statistic *ReadStructFromDatabase()
-{
 	unsigned int structCount = StructCount();
-	Statistic *statistic = new Statistic[structCount];
 
-	std::ifstream file(FILE_NAME);
-
-	char buff[20];
-
-	for (int i(0); i < structCount; ++i)
+	if (structCount == 0)
 	{
-		file >> buff >> statistic[i].InternetProtocol >> statistic[i].ConnectTime >> buff;
-		
-		statistic[i].ConnectTime[8] = ' ';
+		if ((file = fopen(FILE_NAME, "w")) == NULL)
+			return false;
 
-		for (int j(0); j < 8; ++j)
-	  	  statistic[i].ConnectTime[j + 9] = buff[j];
+		WriteStructInFstream(file, oneStruct);
 
-		statistic[i].ConnectTime[17] = '\0';
+		if (fclose(file) == EOF)
+			return false;
 
-		file >> statistic[i].CountReceiveDate >> statistic[i].CountSendDate >> buff;
+		return true;
 	}
 
-	return statistic;
+	Statistic *statistic = ReadStructFromDatabase();
+
+	for (int i(0); i < structCount; ++i)
+		if ((std::string(oneStruct.InternetProtocol) == std::string(statistic[i].InternetProtocol)))
+		{
+			statistic[i] = oneStruct;
+
+			PutAllInDatabase(statistic, structCount);
+
+			return true;
+		}
+		
+	if ((file = fopen(FILE_NAME, "a")) == NULL)
+		return NULL;
+
+	WriteStructInFstream(file, oneStruct);
+
+	if (fclose(file) == EOF)
+		return false;
+
+	return true;
 }
 
 std::string ReadStringFromDatabase()
@@ -94,4 +157,13 @@ std::string ReadStringFromDatabase()
 	return resultString;
 }
 
+int main()
+{
+	Statistic oneStruct = { "363.123.123.123", "06:25:16 02.11.17", 42, 24 };
 
+	PutInDatabase(oneStruct);
+
+	int buff;
+
+	return 0;
+}

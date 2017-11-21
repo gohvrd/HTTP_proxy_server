88e1a28fcca49c03aaa66b620b26758fff95ed2c
LakrimozaSL <Reqiman@mail.ru>
2017-11-21 23:23:28 +0300

    Add files via upload

--- database/Database.cpp
+++ database/Database.cpp
@@ -1,5 +1,7 @@
-#include 
+#include "stdafx.h"
 #include <iostream>
+#include <string>
+#include <fstream>
 
 #include "Statistic.hpp"
 
@@ -34,42 +36,62 @@ bool PutInDatabase(Statistic *statistic, unsigned int count)
 }
 
 
-/* В разработке
-Statistic *ReadOnDatabase()
+unsigned int StructCount()
 {
 	FILE *file;
 
-	if ((file = fopen(FILE_NAME, "r")) == NULL)
+	if ((file = fopen(FILE_NAME, "rt")) == NULL)
 		return NULL;
 
 	unsigned int structCount(0);
 	char c;
 
-	while (!feof(file)) 
-		if ((c = fgetc(file)) == '{' || c == EOF)
+	while (!feof(file))
+		if ((c = fgetc(file)) == '{')
 			structCount++;
 
-	
-	unsigned int structCount(0);
+	return structCount;
+}
 
-	if (stringCount != 0)
-		structCount = (unsigned int)((stringCount - 1) / 6);
-	
+Statistic *ReadStructFromDatabase()
+{
+	unsigned int structCount = StructCount();
 	Statistic *statistic = new Statistic[structCount];
 
-	wchar_t buff[5];
+	std::ifstream file(FILE_NAME);
 
-	for (unsigned int i(0); i < structCount; ++i)
+	char buff[20];
+
+	for (int i(0); i < structCount; ++i)
 	{
-		fwscanf(file, L"%s", buff);
-		std::cout << buff;
-		std::cin >> c;
+		file >> buff >> statistic[i].InternetProtocol >> statistic[i].ConnectTime >> buff;
+		
+		statistic[i].ConnectTime[8] = ' ';
+
+		for (int j(0); j < 8; ++j)
+	  	  statistic[i].ConnectTime[j + 9] = buff[j];
+
+		statistic[i].ConnectTime[17] = '\0';
+
+		file >> statistic[i].CountReceiveDate >> statistic[i].CountSendDate >> buff;
 	}
 
-	return NULL;
+	return statistic;
 }
-*/
+
+std::string ReadStringFromDatabase()
+{
+	std::string resultString, buffString;
 
 
+	Statistic* statistic = ReadStructFromDatabase();
+
+	unsigned int structCount = StructCount();
+
+	for (int i(0); i < structCount; ++i)
+		resultString = resultString + std::string(statistic[i].InternetProtocol) + "?" + std::string(statistic[i].ConnectTime) + "?" + std::to_string(statistic[i].CountReceiveDate) + "?" + std::to_string(statistic[i].CountSendDate) + "!!!";
+	
+	return resultString;
+}
 
 
